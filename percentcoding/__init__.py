67889deb2298dc5e049055e5a0bf51caea86fdf0
Alan Grow <alangrow@gmail.com>
2011-03-07 01:40:53 -0500

    export standard url encoding via quote and unquote

--- percentcoding/__init__.py
+++ percentcoding/__init__.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
 
 from percentcoding.cext import *
+import string
+
+URI_UNRESERVED = string.uppercase + string.lowercase + string.digits + "-_.~"
+URI_RESERVED = "!*'();:@&=+$,/?#[]"
+URI_SAFE = ''.join( set([c for c in URI_UNRESERVED]) - set([c for c in URI_RESERVED]) )
+URI_SAFE_FORM = URI_SAFE.replace('+','')
+
+urlcoding = Codec(URI_SAFE)
+
+def quote(s):
+  global urlcoding
+  return urlcoding.encode(s)
+
+def unquote(s):
+  global urlcoding
+  return urlcoding.decode(s)
 
