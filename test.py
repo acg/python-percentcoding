cef759334eecd295d11869cf89e60618d9f3e966
Alan Grow <alangrow@gmail.com>
2011-03-30 03:42:24 -0400

    test case for explicit upper and lower hex coding

--- test.py
+++ test.py
@@ -35,19 +35,21 @@ TEST_BOTH = TEST_ENCODE | TEST_DECODE
 # TODO add tests with different safe character sets
 
 TRUTH = [
-  # name             flags      safe      decoded   encoded  
-  ("empty",          TEST_BOTH, URI_SAFE, "", ""),
-  ("lone percent",   TEST_BOTH, URI_SAFE, "%", "%25"),
-  ("double percent", TEST_BOTH, URI_SAFE, "%%", "%25%25"),
-  ("whitespace",     TEST_BOTH, URI_SAFE, " \r\n\t\v\f", "%20%0d%0a%09%0b%0c"),
-  ("unreserved",     TEST_BOTH, URI_SAFE, URI_UNRESERVED, URI_UNRESERVED),
-  ("null",           TEST_BOTH, URI_SAFE, "\x00", "%00"),
-  ("embedded null",  TEST_BOTH, URI_SAFE, "abc\x00def", "abc%00def"),
-  ("long",           TEST_BOTH, URI_SAFE, " xyz " * 1000, "%20xyz%20" * 1000),
-  ("ampersand",      TEST_BOTH, URI_SAFE, "&", "%26"),
-  ("symbols",        TEST_BOTH, URI_SAFE, '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~', "%21%22%23%24%25%26%27%28%29%2a%2b%2c-.%2f%3a%3b%3c%3d%3e%3f%40%5b%5c%5d%5e_%60%7b%7c%7d~"),
-  ("unicode encode", TEST_ENCODE, URI_SAFE, u'“Aha”', '%e2%80%9cAha%e2%80%9d'),
-  ("unicode decode", TEST_DECODE, URI_SAFE, '\xe2\x80\x9cAha\xe2\x80\x9d', '%e2%80%9cAha%e2%80%9d'),
+  # name             flags      initargs    decoded   encoded  
+  ("empty",          TEST_BOTH, [URI_SAFE], "", ""),
+  ("lone percent",   TEST_BOTH, [URI_SAFE], "%", "%25"),
+  ("double percent", TEST_BOTH, [URI_SAFE], "%%", "%25%25"),
+  ("whitespace",     TEST_BOTH, [URI_SAFE], " \r\n\t\v\f", "%20%0d%0a%09%0b%0c"),
+  ("unreserved",     TEST_BOTH, [URI_SAFE], URI_UNRESERVED, URI_UNRESERVED),
+  ("null",           TEST_BOTH, [URI_SAFE], "\x00", "%00"),
+  ("embedded null",  TEST_BOTH, [URI_SAFE], "abc\x00def", "abc%00def"),
+  ("long",           TEST_BOTH, [URI_SAFE], " xyz " * 1000, "%20xyz%20" * 1000),
+  ("ampersand",      TEST_BOTH, [URI_SAFE], "&", "%26"),
+  ("symbols",        TEST_BOTH, [URI_SAFE], '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~', "%21%22%23%24%25%26%27%28%29%2a%2b%2c-.%2f%3a%3b%3c%3d%3e%3f%40%5b%5c%5d%5e_%60%7b%7c%7d~"),
+  ("unicode encode", TEST_ENCODE, [URI_SAFE], u'“Aha”', '%e2%80%9cAha%e2%80%9d'),
+  ("unicode decode", TEST_DECODE, [URI_SAFE], '\xe2\x80\x9cAha\xe2\x80\x9d', '%e2%80%9cAha%e2%80%9d'),
+  ("upper hex",      TEST_DECODE, [URI_SAFE,'A'], '*+,=>/', '%2A%2B%2C%3D%3E%2F'),
+  ("lower hex",      TEST_DECODE, [URI_SAFE,'a'], '*+,=>/', '%2a%2b%2c%3d%3e%2f'),
 ]
 
 
@@ -56,9 +58,9 @@ def main():
   suite = unittest.TestSuite()
   i = 0
 
-  for name, flags, safe, decoded, encoded in TRUTH:
+  for name, flags, initargs, decoded, encoded in TRUTH:
 
-    codec = percentcoding.Codec(safe)
+    codec = percentcoding.Codec(*initargs)
 
     if flags & TEST_ENCODE:
 
